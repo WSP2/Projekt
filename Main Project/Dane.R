19f2e4dd22e1b979a60f46a99765c86cff32129f
Tomridders <filmac92@gmail.com>
2015-04-28 00:18:04 +0200

    Laboratoria nr 1
    To sa pliki które na wstepie stworzylem do naszego repo. Co do hosta to
    pogadamy na zajeciach, pozdro

---
+++ Main Project/Dane.R
@@ -0,0 +1,8 @@
+# TODO: Add comment
+# 
+# Author: Filip
+##############################################################################
+library(R.matlab)
+pathname <- file.path("C:/Users/Filip/Documents/R//Projekt/Data", "genes_list.mat")
+data <- readMat(pathname) 
+

54454dec6e73b7aa3c72d5c7e0023633e4eb3ba8
DariaKogut <daria.kogut0@gmail.com>
2015-09-22 15:57:39 +0200

    Dodana heatmapa, będzie poprawiona

--- stat_daru.R
+++ stat_daru.R
@@ -1,19 +1,32 @@
-#to jest tylko proba 
 
-testy_stat<- function(ExprSet,t) {
-  
+#testy_stat<- function(ExprSet,t){
+
 #wyodr?bnienie dw?ch grup
 
   dataRMA = t(dataRMA)
   #carcinoid
-  dane2_t=dataRMA[,(1:15)]
+ dane2_t = matrix(0,15,3)
+   for (i in 1:30){ 
+     for (j in 1:3){
+    if (results[i, 3]==1)
+  dane2_t[i,j]= results[i,j]
   #normal
-  dane2_t2 = dataRMA[,(16:30)]
-   
+  else
+  dane2_t2[i,j] = results[i,j]
+     }
+}
+
 #prawilny test t dla prob niezale?nych
+
   var.test(dane2_t,dane2_t2) #sprawd?my czy jest r??norodno?? wariancji
   t = t.test(dane2_t,dane2_t2)
-}
+  #wilcoxon?
+  
 wyniki = testy_stat(ExprSet, t)
 #write.table(file='testy_statystyczne.txt', wyniki)
-#coś mi nie commituje
+
+#heatmapa
+
+jpeg(filename="heatmap_")
+hmp= heatmap(dane2_t, dane2_t2, Rowv=NULL)
+dev.off()
